e4766f8d6f91d5f98515b83808931a656ef8bbe3
AlejandraKeber <alejandrakeber@gmail.com>
2023-04-04 15:53:02 -0500

    Video 14: Save products info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-get '/products/new', to: 'products#new', as: :new_product
-get '/products', to: 'products#index'
-get '/products/:id', to: 'products#show', as: :product
+  post '/products', to: 'products#create'
+  get '/products/new', to: 'products#new', as: :new_product
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
 end
