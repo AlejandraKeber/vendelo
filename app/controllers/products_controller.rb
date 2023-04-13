3ffe750b08d5686d1a865b79bb197acb987c3d1f
AlejandraKeber <alejandrakeber@gmail.com>
2023-04-13 09:28:08 -0500

    Video 19: Delete products with destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente'
+  end
+
   private
   def product_params
     params.require(:product).permit(:title, :description, :price)
