dd4f0d76b43afed7ec0a754a2c35e84789006bcc
AlejandraKeber <alejandrakeber@gmail.com>
2023-04-28 14:41:02 -0500

    Video 40: Save favorites

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,13 @@
+class FavoritesController < ApplicationController
+  def create
+    Favorite.create(product: product, user: Current.user)
+    redirect_to product_path(product)
+  end
+
+  private
+
+  def product
+    @product ||= Product.find(params[:product_id])
+  end
+   
+end
